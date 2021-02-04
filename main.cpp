de0e34a845493ee6232503317689a7fc9c780e90
LaZorraTech <74510424+LaZorraTech@users.noreply.github.com>
2021-02-03 20:38:59 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+#define TFT_GREY 0x5AEB // New colour
+
+#define TFT_DC   18  // Data Command control pin - must use a pin in the range 0-31  (LCD_RS on my display)
+#define TFT_CS   33  // Chip select control pin
+#define TFT_RST  32  // Reset pin
+#define TFT_RD    2
+#define TFT_WR    4  // Write strobe control pin - must use a pin in the range 0-31
+
+#define TFT_D0   12  // Must use pins in the range 0-31 for the data bus
+#define TFT_D1   13  // so a single register write sets/clears all bits
+#define TFT_D2   26
+#define TFT_D3   25
+#define TFT_D4   21
+#define TFT_D5   5
+#define TFT_D6   27
+#define TFT_D7   14
+
+#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+
+#define SMOOTH_FONT
+
+void setup() {
+  tft.init();
+  tft.setRotation(1);
+}
+
+void loop() {
+  // Fill screen with random colour so we can see the effect of printing with and without 
+  // a background colour defined
+  tft.fillScreen(random(0xFFFF));
+  
+  // Set "cursor" at top left corner of display (0,0) and select font 2
+  // (cursor will move to next line automatically during printing with 'tft.println'
+  //  or stay on the line is there is room for the text with tft.print)
+  tft.setCursor(0, 0, 2);
+  // Set the font colour to be white with a black background, set text size multiplier to 1
+  tft.setTextColor(TFT_WHITE,TFT_BLACK);  tft.setTextSize(1);
+  // We can now plot text on screen using the "print" class
+  tft.println("Hello World!");
+  
+  // Set the font colour to be yellow with no background, set to font 7
+  tft.setTextColor(TFT_YELLOW); tft.setTextFont(7);
+  tft.println(1234.56);
+  
+  // Set the font colour to be red with black background, set to font 4
+  tft.setTextColor(TFT_RED,TFT_BLACK);    tft.setTextFont(4);
+  tft.println((long)3735928559, HEX); // Should print DEADBEEF
+
+  // Set the font colour to be green with black background, set to font 4
+  tft.setTextColor(TFT_GREEN,TFT_BLACK);
+  tft.setTextFont(4);
+  tft.println("Groop");
+  tft.println("I implore thee,");
+
+  // Change to font 2
+  tft.setTextFont(2);
+  tft.println(F("my foonting turlingdromes.")); // Can store strings in FLASH to save RAM
+  tft.println("And hooptiously drangle me");
+  tft.println("with crinkly bindlewurdles,");
+  // This next line is deliberately made too long for the display width to test
+  // automatic text wrapping onto the next line
+  tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
+  
+  // Test some print formatting functions
+  float fnumber = 123.45;
+   // Set the font colour to be blue with no background, set to font 4
+  tft.setTextColor(TFT_BLUE);    tft.setTextFont(4);
+  tft.print("Float = "); tft.println(fnumber);           // Print floating point number
+  tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
+  tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
+  delay(10000);
+}
